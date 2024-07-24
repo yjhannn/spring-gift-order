9146e072f3e490ad4ed6c6cfb321ac14629a22fc
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-24 12:35:44 +0900

    feat : KakaoResponse 추가

--- src/main/java/gift/kakaologin/KakaoResponse.java
+++ src/main/java/gift/kakaologin/KakaoResponse.java
@@ -1,2 +1,18 @@
-package gift.kakaologin;public class KakaoResponse {
+package gift.kakaologin;
+
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(value = PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class KakaoResponse {
+    private String tokenType;
+    private String accessToken;
+    private String refreshToken;
+    private Integer expiresIn;
+    private Integer refreshTokenExpiresIn;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
 }
