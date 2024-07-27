74a4d44e4bf587713d037b64a8da1cec0581f5c7
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-07-28 00:19:30 +0900

    refactor : 사용하지 않는 의존성 제거

---
+++ src/main/java/gift/common/config/SwaggerConfig.java
@@ -0,0 +1,2 @@
+package gift.common.config;public class SwaggerConfig {
+}
--- src/main/java/gift/service/OrderService.java
+++ src/main/java/gift/service/OrderService.java
@@ -22,19 +22,17 @@ public class OrderService {
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
     private final WishListRepository wishListRepository;
-    private final KakaoService kakaoService;
     private final KakaoMessageService kakaoMessageService;
 
     public OrderService(OrderRepository orderRepository, OptionRepository optionRepository,
         UserRepository userRepository, ProductRepository productRepository,
-        WishListRepository wishListRepository, KakaoService kakaoService,
+        WishListRepository wishListRepository,
         KakaoMessageService kakaoMessageService) {
         this.orderRepository = orderRepository;
         this.optionRepository = optionRepository;
         this.userRepository = userRepository;
         this.productRepository = productRepository;
         this.wishListRepository = wishListRepository;
-        this.kakaoService = kakaoService;
         this.kakaoMessageService = kakaoMessageService;
     }
 
